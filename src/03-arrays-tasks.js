cae03e116aa54d4bbef59b832a8aee68f3a4a84f
Oleksii Korovushkin <alexkrv07@gmail.com>
2022-01-24 21:56:54 +0200

    feat: solve array

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -380,8 +380,13 @@ function getItemsSum(arr) {
  *  [ -1, 'false', null, 0 ] => 2
  *  [ null, undefined, NaN, false, 0, '' ]  => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+  const result = arr.filter((item) => !item);
+  return result.length;
+  // throw new Error('Not implemented');
 }
 
 /**
@@ -398,8 +403,10 @@ function getFalsyValuesCount(/* arr */) {
  *    [ null, undefined, null ], null => 2
  *    [ true, 0, 1, 'true' ], true => 1
  */
-function findAllOccurrences(/* arr, item */) {
-  throw new Error('Not implemented');
+function findAllOccurrences(arr, item) {
+  const result = arr.filter((itemArr) => itemArr === item);
+  return result.length;
+  // throw new Error('Not implemented');
 }
 
 /**
@@ -413,8 +420,9 @@ function findAllOccurrences(/* arr, item */) {
  *    [1, 2, 3, 4, 5]                   => '1,2,3,4,5'
  *    ['rock', 'paper', 'scissors']     => 'rock,paper,scissors'
  */
-function toStringList(/* arr */) {
-  throw new Error('Not implemented');
+function toStringList(arr) {
+  return arr.join(',');
+  // throw new Error('Not implemented');
 }
 
 
